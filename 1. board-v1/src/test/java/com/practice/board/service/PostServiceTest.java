4b54c76097cfd3381f51fd51e4bdc2a09a9aa362
simgyuhwan <rbghks19@naver.com>
2022-06-29 18:00:18 +0900

    refactor: 테스트코드 삭제

--- 1. board-v1/src/test/java/com/practice/board/service/PostServiceTest.java
+++ 1. board-v1/src/test/java/com/practice/board/service/PostServiceTest.java
@@ -17,45 +17,6 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.BDDMockito.*;
 
-@ExtendWith(MockitoExtension.class)
 class PostServiceTest {
-
-    @InjectMocks
-    PostService service;
-
-    @Mock
-    PostRepository repository;
-
-    @Test
-    @DisplayName("게시물 등록 테스트")
-    void savePostTest(){
-        // given
-        Posts posts = createPosts();
-        given(repository.save(posts)).willReturn(posts);
-
-        // when
-        service.register(posts);
-
-        // then
-        verify(repository).save(any(Posts.class));
-    }
-
-    public List<Posts> createPostList(){
-        List<Posts> posts = new ArrayList<>();
-        for(int i=0; i<3; i++){
-            posts.add(
-                    Posts.builder()
-                            .title("title" + i)
-                            .content("content" + i)
-                            .writer("writer" + i)
-                            .build()
-            );
-        }
-        return posts;
-    }
-
-    public Posts createPosts(){
-        return PostFactory.createPosts();
-    }
 
 }
