944b5189f16dd0945d96297e11629f5be8b7cea8
simgyuhwan <rbghks19@naver.com>
2022-07-08 11:28:14 +0900

    feat: imgFile controller 전송 확인

--- 1. board-v1/src/main/java/com/practice/board/domain/post/controller/PostController.java
+++ 1. board-v1/src/main/java/com/practice/board/domain/post/controller/PostController.java
@@ -16,9 +16,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import java.security.Principal;
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -49,7 +51,9 @@ public class PostController {
     }
 
     @PostMapping("/post")
-    public String write(HttpServletRequest request, Model model, @Validated PostDto postDto){
+    public String write(HttpServletRequest request, Model model, @Validated PostDto postDto,
+                        @RequestPart("imgFile") List<MultipartFile> imgFileList){
+        imgFileList.size();
         model.addAttribute("PostDto", PostDto.of(service.addPosts(postDto)));
         return "redirect:/board";
     }
